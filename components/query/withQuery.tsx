32a070915f3720c193abe27961d666c5c6f6f5fc
carney520 <carney520@hotmail.com>
2018-11-02 09:38:30 +0800

    新增withQuery方法

---
+++ components/query/withQuery.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Omit } from 'utils/type-utils'
+import Query, { QueryComponentProps } from './Query'
+
+export default function withQuery<P extends QueryComponentProps>(
+  Component: React.ComponentType<P>,
+): React.ComponentClass<Omit<P, keyof QueryComponentProps>> {
+  return class extends React.Component<Omit<P, keyof QueryComponentProps>> {
+    public render() {
+      return <Query>{props => <Component {...props} {...this.props} />}</Query>
+    }
+  }
+}
