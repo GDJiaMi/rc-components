f9f54287bf423283694a645a971841fabadf3053
carney520 <carney520@hotmail.com>
2018-11-02 15:52:28 +0800

    修复Currency组件类型

--- components/currency/index.tsx
+++ components/currency/index.tsx
@@ -1,7 +1,7 @@
 /**
  * 货币，数字格式化
  */
-import React, { StatelessComponent, HTMLAttributes } from 'react'
+import React, { HTMLAttributes } from 'react'
 
 export interface CurrencyProps extends HTMLAttributes<HTMLSpanElement> {
   precision?: number
@@ -12,15 +12,15 @@ export interface CurrencyProps extends HTMLAttributes<HTMLSpanElement> {
 
 const ThousandRegex = /\d(?=(\d{3})+\.)/g
 
-const Currency: StatelessComponent<CurrencyProps> = (props: CurrencyProps) => {
-  const { suffix, value, precision, prefix, ...other } = props
-  return (
-    <span {...other}>
-      {(prefix || '') +
-        value.toFixed(precision || 2).replace(ThousandRegex, '$&,') +
-        (suffix || '')}
-    </span>
-  )
+export default class Currency extends React.PureComponent<CurrencyProps> {
+  public render() {
+    const { suffix, value, precision, prefix, ...other } = this.props
+    return (
+      <span {...other}>
+        {(prefix || '') +
+          value.toFixed(precision || 2).replace(ThousandRegex, '$&,') +
+          (suffix || '')}
+      </span>
+    )
+  }
 }
-
-export default Currency
